0cb6cfeb16ea330604753b295d7772c4f3eee8fc
Lorena Delgado Moroni <61249541+LorenaDM@users.noreply.github.com>
2022-03-30 21:13:50 -0300

    ok

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,75 +1,72 @@
-import { useRouter } from 'next/router';
-import { GetStaticPaths, GetStaticProps } from 'next/types';
-
-//cria um tipo de dado para comentário
+import { GetStaticPaths, GetStaticProps } from 'next'
+import {useRouter} from 'next/router'
+// cria um tipo de dados para comentário
 interface Comment {
-  id: string;
-  body: string;
+    id: string,
+    body: string
 }
-//Cria um vetor de dados de vetor de comentários
+// cria um tipo de dados de vetor de comentários
 interface CommentArray {
-  comments: Comment[];
+    comments: Comment[]
 }
-export default function Post({ comments }: CommentArray) {
-  const router = useRouter();
-  if (router.isFallback) {
-    //estiver cmo true
-    return <p>Loading...</p>;
-  }
 
-  return (
-    <>
-      <h1> Post {router.query.id}</h1>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}> {comment.body} </li>
-        ))}
-      </ul>
-    </>
-  );
+export default function Post({comments}: CommentArray){
+    const router = useRouter()
+    if(router.isFallback) { // estiver como true
+        return <p> Loading ... </p>
+    }
+    return (
+        <>
+            <h1> Post {router.query.id}</h1>
+            <ul>
+                {
+                    comments.map(comment => (
+                        <li key={comment.id}> {comment.body} </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
 }
-//singular pois vai procurar uma unidade
-//gera o conteudo das paginas para cada post
-//context contem tds os ids dos posts
-export const getStaticProps: GetStaticProps<CommentArray> = async (
-  //vai ser executado antes de carregar o componente
-  //recupera todos os posts
-  context,
-) => {
-  //recupera id do post
-  const { id } = context.params;
-  //faz consulta
-  const response = await fetch(`http://localhost:3333/comments?postId=${id}`);
-  //converte
-  const comments = await response.json();
 
-  return {
-    props: {
-      comments,
-    }, // will be passed to the page component as props
-    revalidate: 5,
-  };
-};
-export const getStaticPaths: GetStaticPaths = async () => {
-  //vamos obter
-  const response = await fetch(`http://localhost:3333/posts`);
-  const posts = await response.json();
-  //a variavrl paths vai conter todos os ids de todos os posts
-
-  const paths = posts.map((post) => {
+// gera o conteúdo das páginas para cada post, consulta os comentários de cada post
+// context contém todos os ids dos posts
+export const getStaticProps: GetStaticProps<CommentArray> = async(context) => {
+    // recupera id do post
+    const {id} = context.params
+    // faz a consulta 
+    const response = await fetch(`http://localhost:3333/comments?postId=${id}`)
+    // converte resposta em json - temos os comentários de um post id
+    const comments = await response.json()
+    // vamos retornar
     return {
-      params: {
-        id: String(post.id),
-      },
-    };
-  });
-  //retorno da função
+        props: {
+            comments
+        },
+        revalidate: 5
+    }
+}
 
-  return {
-    paths,
-    //não era aas paginaas, apenas no primeiro acesso
-    //paths: paths, gera uma página estática para cd post
-    fallback: true, //false - retorna 404 quando não tem a página
-    //true - cria página automaticamente
-  };
-};
+// vai ser executado antes de carregar o componente
+// recupera todos os posts
+export const getStaticPaths:GetStaticPaths = async() => {
+    // vamos obter os posts
+    //const response = await fetch(`http://localhost:3333/posts`)
+    //const posts = await response.json()
+    // a variável paths vai conter todos os ids de todos os posts
+    //const paths = posts.map( post => {
+    //    return {
+    //            params: {
+    //                id: String(post.id) // chama getStaticProps
+    //            }
+    //        }
+    //    })
+    // retorno da função
+    return {
+        paths: [], // não gera as páginas
+        // não gera páginas, apenas no primeiro acesso
+        // inicia a geração de cada página estática para cada post
+        fallback: true // false - retorna 404 quando não tem a página
+        // true - cria página automaticamente
+    }
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,28 +1,33 @@
-import { GetStaticProps } from 'next';
-interface Post {
-  // representa 1 post
-  id: string;
-  title: string;
+import {GetStaticProps} from 'next'
+interface Post { // representa 1 post
+    id: string;
+    title: string;
+}
+  
+interface Posts { // representa vetor de posts
+    posts: Post[]
 }
 
-interface Posts {
-  // representa vetor de posts
-  posts: Post[];
+export default function Posts({posts}: Posts){
+    return (
+        <div>
+            <h1> Posts </h1>
+                <ul>
+                {posts.map( post => (
+                    <li key={post.id}> {post.title} </li>
+                ))}
+            </ul>
+    </div>
+    )
 }
 
-export default function Posts() {
-  return <h1> Posts </h1>;
-}
-
-export const getStaticProps: GetStaticProps<Posts> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
+export const getStaticProps: GetStaticProps<Posts> = async() => {
+  const response = await fetch('http://localhost:3333/posts')
+  const posts = await response.json()
   return {
     props: {
-      posts, // posts vai para o componente
+      posts // posts vai para o componente
     },
-  };
-};
-//solução para sites que não atualizados frequentementes
-//revalidate monta a pagina estática uma vez e de 5 em 5 tempos cria a pág de nvo
-//npm run build - construção otimizada
+    revalidate: 5000
+  }
+}
